4f9c363c7fdf7e86eec0badd46b4e3515e42f137
xxaksed <aksed.336@mail.ru>
2024-01-26 19:02:31 +0300

    DropDown

--- main.js
+++ main.js
@@ -1,56 +1,116 @@
-const button = document.getElementById("start")
-const timer = document.getElementById("time")
-let time = 0
-let timerInterval
-let timerActive = false;
+const data = [
+	{ name: "Yakidoo" },
+	{ name: "Flashspan" },
+	{ name: "Jayo" },
+	{ name: "Livetube" },
+	{ name: "Cogibox" },
+	{ name: "Realbridge" },
+	{ name: "Feedbug" },
+	{ name: "Skidoo" },
+	{ name: "Dabshots" },
+	{ name: "Meeveo" },
+	{ name: "Yoveo" },
+	{ name: "Tavu" },
+	{ name: "Gigashots" },
+	{ name: "Dabtype" },
+	{ name: "Fanoodle" },
+	{ name: "Wordpedia" },
+	{ name: "Pixonyx" },
+	{ name: "Skinix" },
+	{ name: "Browsetype" },
+	{ name: "Quamba" },
+	{ name: "Uganda" },
+	{ name: "Ireland" },
+	{ name: "Poland" },
+	{ name: "Azerbaijan" },
+	{ name: "China" },
+	{ name: "Russia" },
+	{ name: "China" },
+	{ name: "France" },
+	{ name: "China" },
+	{ name: "Sweden" },
+	{ name: "Indonesia" },
+	{ name: "Albania" },
+	{ name: "Russia" },
+	{ name: "Botswana" },
+	{ name: "Indonesia" },
+	{ name: "Mauritius" },
+	{ name: "Brazil" },
+	{ name: "Portugal" },
+	{ name: "France" },
+	{ name: "Azerbaijan" },
+	{ name: "Filipino" },
+	{ name: "Icelandic" },
+	{ name: "Khmer" },
+	{ name: "Macedonian" },
+	{ name: "Kyrgyz" },
+	{ name: "Tsonga" },
+	{ name: "Albanian" },
+	{ name: "Swati" },
+	{ name: "Czech" },
+	{ name: "Dzongkha" },
+	{ name: "Tswana" },
+	{ name: "Khmer" },
+	{ name: "Punjabi" },
+	{ name: "Hindi" },
+	{ name: "English" },
+	{ name: "Dari" },
+	{ name: "Italian" },
+	{ name: "Luxembourgish" },
+	{ name: "Marathi" },
+	{ name: "Lithuanian" },
+	{ name: "newsvine.com" },
+	{ name: "seesaa.net" },
+	{ name: "opensource.org" },
+	{ name: "webmd.com" },
+	{ name: "census.gov" },
+	{ name: "tinyurl.com" },
+	{ name: "bing.com" },
+	{ name: "blogtalkradio.com" },
+	{ name: "jugem.jp" },
+	{ name: "ow.ly" },
+	{ name: "mail.ru" },
+	{ name: "meetup.com" },
+	{ name: "netscape.com" },
+	{ name: "lulu.com" },
+	{ name: "simplemachines.org" },
+	{ name: "google.ru" },
+	{ name: "earthlink.net" },
+	{ name: "privacy.gov.au" },
+	{ name: "loc.gov" },
+	{ name: "pcworld.com" },
+];
 
-function toggleTimer() {
-    timerActive = !timerActive
-    if (timerActive == true) {
-        button.innerText = "stop"
-        startTimer()
-    }
-    else {
-        button.innerText = "start"
-        stopTimer()
-    }
+let search = document.getElementById("search");
+let showResults = document.getElementById("showResults");
+
+// for (let i = 0; i < data.length; i++) {
+// 	if (i == searchValue){
+// 		console.log("gogogog")
+// 	}
+//   }
+
+search.addEventListener("input", function () {
+	let searchValue = search.value.toLowerCase();
+
+	let filteredResults = data.filter(function (item) {
+		return item.name.toLowerCase().startsWith(searchValue);
+	});
+	if (searchValue == "") {
+		displayResults("");
+	}
+
+	// console.log(startsWith);
+	displayResults(filteredResults);
+});
+
+function displayResults(results) {
+	showResults.innerHTML = "";
+
+	results.forEach(function (results) {
+		let resultsElement = document.createElement("div");
+		resultsElement.classList.add("searchResult");
+		resultsElement.textContent = results.name;
+		showResults.appendChild(resultsElement);
+	});
 }
-
-function debounce(callback, delay) {
-    let timeout;
-    return function () {
-        clearTimeout(timeout);
-        timeout = setTimeout(callback, delay);
-    }
-}
-
-const deb = debounce(toggleTimer, 1000)
-
-function incrementTimer() {
-    time++
-    timer.innerText = time
-}
-function startTimer() {
-    timerInterval = setInterval(incrementTimer, 1000);
-}
-
-function stopTimer() {
-    clearInterval(timerInterval)
-}
-
-button.onclick = function () {
-    deb()
-}
-
-let colors = ["blue", "white", "red", "green", "gray", "aqua", "brown", "azure", "blueviolet"]
-
-function getRandomColor(){
-    let rand = Math.floor(Math.random() * colors.length)
-    return colors[rand]
-}
-setInterval(setRandomButtonColor, 3000)
-
-function setRandomButtonColor(){
-    button.style.backgroundColor = getRandomColor()
-}
-
